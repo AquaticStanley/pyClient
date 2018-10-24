66686ab23ad6e05b9b81f059d0b9781e07a686fd
Tristan Fountain <tristan@Tristans-MacBook-Pro.local>
2018-10-23 22:18:33 -0500

    Added support to connect by IP

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ import threading
 HOST = ""
 PORT = 0000
 USERNAME = ''
+IP = "0.0.0.0"
 MAX_MESSAGE_LENGTH = 768
 MAX_USERNAME_LENGTH = 256
 
@@ -13,12 +14,16 @@ def assignConstants():
   global HOST
   global PORT
   global USERNAME
+  global IP
   config = open("Profile.cfg")
   config.readline()
   USERNAME = config.readline().strip()
   config.readline()
   config.readline()
   HOST = config.readline().strip()
+  config.readline()
+  config.readline()
+  IP = config.readline().strip()
   config.readline()
   config.readline()
   PORT = int(config.readline().strip())
@@ -42,7 +47,7 @@ print("Success!")
 
 # Connect to host
 print("Connecting to host...", end="")
-client_socket.connect((HOST, PORT))
+client_socket.connect((IP, PORT))
 print("Success!")
 
 # Send username to use
@@ -60,5 +65,6 @@ while True:
   else:
     client_socket.send(data.encode())
 
+print("Connection will now be closed...")
 client_socket.close()
-print("Disconnected")
+print("Connection is now closed.")
